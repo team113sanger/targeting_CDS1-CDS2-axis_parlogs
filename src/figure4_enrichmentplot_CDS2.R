d5b2143e50b7227ae494375c3526e736c2c0c93b
jb63 <jb63@sanger.ac.uk>
2024-07-31 09:23:52 +0000

    Update gitattributes

--- src/figure4_enrichmentplot_CDS2.R
+++ src/figure4_enrichmentplot_CDS2.R
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b5e2d8a7b5753ca2602b5ea92600e00286fd4a47a33f6172d05fde5a94da63db
-size 1177
+# July 2024
+# Figure 4 - Enrichment dot plot CDS2
+
+library(tidyverse)
+library(here)
+
+# Load pathway results
+CDS2 <- read.csv("CDS2_Pathway.csv", header = TRUE, sep = "\t")
+
+
+# Order and plot
+CDS2_ord <- CDS2 |> 
+            mutate(pathway = fct_reorder(Pathway, Fold_Enrichment))
+
+p <- ggplot(CDS2_ord, aes(x = Fold_Enrichment, y = pathway)) +
+  labs(x = "Fold Enrichment", y = "", title = "Gene Set Enrichment Analysis") +
+  geom_point(size = 5.9, color = "blue") +
+  theme_classic() +
+  theme(
+    axis.title.x = element_text(size = 14, color = "black", face = "bold"),
+    axis.text.x = element_text(size = 13, face = "bold", color = "black"),
+    axis.text.y = element_text(face = "bold", size = 15, color = "black", vjust = 0.5),
+    axis.title.y = element_text(lineheight = 2, color = "black", size = 14),
+    plot.title = element_text(face = "bold", size = 18),
+    axis.line = element_line(color = "black"),
+    axis.title = element_text(size = 13, color = "black", face = "bold")
+  ) +
+  scale_x_continuous(limits = c(0, 100), breaks = seq(0, 100, by = 20))
+
+# Rbase save PDF
+pdf(here("results/plots/CDS2_enrichment17.pdf"), width = 17, height = 10)
+plot(p)
+dev.off()
--- src/figure5_waterfall.R
+++ src/figure5_waterfall.R
@@ -1,3 +1,93 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2e39c02d487bec41265addc06b9f68048058872f89ab8bda0547e483e90ad73a
-size 2978
+# 27th June 2024
+
+# this script aims to generate a dataframe to replicate the Waterfall plot in Jen's Paper
+
+library(ggplot2)
+library(tidyverse)
+library(here)
+
+# Import the data.These are all from 23Q4 release of DepMap
+
+# RNA_Seq data
+OmicsExpressionProteinCodingGenesTPMLogp1_23Q4_ <- read_csv(here("data/OmicsExpressionProteinCodingGenesTPMLogp1(23Q4).csv"))
+
+# CRISPR Dependency Data
+CRISPRGeneEffect_23Q4 <- read_csv(here(
+    "data/CRISPRGeneEffect_23Q4.csv"))
+
+# Models file
+Model_23Q4 <- read_csv(here("data/Model_23Q4.csv"))
+
+# Select the columns of interest
+
+RNA_Seq_CDS1_23Q4_CDS1 <- OmicsExpressionProteinCodingGenesTPMLogp1_23Q4_ |> select(`...1`, `CDS1 (1040)`)
+CRISPRGeneEffect_23Q4_CDS2 <- CRISPRGeneEffect_23Q4 |> 
+                              select(`...1`, `CDS2 (8760)`)
+Model_23Q4_2 <- Model_23Q4 |> 
+                select(ModelID, DepmapModelType)
+
+# add an extra column to the dataframe so that it can be merged
+
+Model_23Q4_3 <- Model_23Q4_2 |> 
+                mutate(...1 = ModelID)
+
+# Merge the dataframes
+
+combined_df_1 <- full_join(Model_23Q4_3, CRISPRGeneEffect_23Q4_CDS2, by = "...1")
+
+final_combined <- full_join(combined_df_1, RNA_Seq_CDS1_23Q4_CDS1, by = "...1")
+
+final_combined_minus_UM <- final_combined |> 
+                            filter(!DepmapModelType == "UM")
+
+
+
+
+write_csv(final_combined_minus_UM, here("results/tables/final_combined_minus_UM.csv"))
+
+# Remove rows with NA values for plotting
+filtered_data <- na.omit(final_combined_minus_UM[, c("CDS2 (8760)", "CDS1 (1040)")])
+
+# filtered_data |> mutate("Colour" = case_when(`CDS1 (1040)` >0 & `CDS1 (1040)` <=1 ~ "A", .default = "X")) #`CDS1 (1040)` >1))
+
+
+filtered_data_2 <- filtered_data |> 
+                   mutate("Colour" = case_when(`CDS1 (1040)` > 0 & `CDS1 (1040)` <= 2 ~ "A", `CDS1 (1040)` > 2 & `CDS1 (1040)` <= 4 ~ "B", `CDS1 (1040)` > 4 & `CDS1 (1040)` <= 6 ~ "C", `CDS1 (1040)` > 6 & `CDS1 (1040)` <= 20 ~ "D", .default = "D")) # `CDS1 (1040)` >1))
+
+
+# Read and preprocess data. This is the same dataframe as provided by Jen but I deleted the cell lines marked as "eye".
+
+# Order the data by CDS2_chronos in descending order
+CDS1_CDS2_df <- filtered_data_2[order(filtered_data_2$`CDS2 (8760)`, decreasing = TRUE), ]
+
+# Map colors based on the 'Colour' column
+color_map <- c(
+  "A" = "#247198",
+  "B" = "#A8C2B9",
+  "C" = "#EACA80",
+  "D" = "#f5af3d",
+  "E" = "#CB6B79",
+  "F" = "#CB6B79"
+)
+CDS1_CDS2_df$Color <- color_map[CDS1_CDS2_df$Colour]
+
+# Create the waterfall plot using ggplot2
+Waterfall <- ggplot(CDS1_CDS2_df, aes(x = reorder(row.names(CDS1_CDS2_df), -`CDS2 (8760)`), y = `CDS2 (8760)`, fill = Colour)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = color_map) +
+  labs(y = "CDS2 Chronos score") +
+  theme_classic()
+
+# Display the plot
+print(Waterfall)
+
+# Save the plot with 600 DPI
+ggsave(
+  filename = here("results/plots/Waterfall.jpg"),
+  plot = Waterfall,
+  device = "jpeg",
+  width = 15,
+  height = 5,
+  units = "in",
+  dpi = 600
+)
